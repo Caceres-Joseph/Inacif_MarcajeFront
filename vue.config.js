42fbf9f5cdcebfdd45fe81114dd3bc6ab1434663
Caceres-Joseph <josephccaceres@gmail.com>
2020-06-30 12:24:45 -0600

    new page

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,12 @@ module.exports = {
             template: 'public/index.html',
             title: 'Home',
             chunks: ['chunk-vendors', 'chunk-common', 'index']
+        },
+        'users': {
+            entry: './src/pages/module01/main.js',
+            template: 'public/index.html',
+            title: 'Users',
+            chunks: ['chunk-vendors', 'chunk-common', 'index']
         }
     }
 }
